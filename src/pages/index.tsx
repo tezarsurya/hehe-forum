06fa6d6b0002ba017a9d3691c9aaa5add2b60177
tezarsurya <tzr.surya@gmail.com>
2023-01-25 14:57:55 +0700

    display user profile info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,31 @@
-import { signIn } from "next-auth/react";
+import { signIn, useSession, signOut } from "next-auth/react";
+import Image from "next/image";
 
 const App = () => {
+  const { status, data } = useSession();
   return (
     <div>
-      <button onClick={() => signIn("github")}>Sign In With Github</button>
+      {status === "authenticated" ? (
+        <button onClick={() => signOut()}>Sign Out</button>
+      ) : status === "loading" ? (
+        <h1>Loading....</h1>
+      ) : (
+        <button onClick={() => signIn("github")}>Sign In With Github</button>
+      )}
+      {status === "authenticated" && (
+        <>
+          <h3>{data?.user?.name || ""}</h3>
+          {data.user?.image && (
+            <Image
+              src={data.user?.image}
+              alt="user avatar"
+              width={24}
+              height={24}
+              priority
+            />
+          )}
+        </>
+      )}
     </div>
   );
 };
