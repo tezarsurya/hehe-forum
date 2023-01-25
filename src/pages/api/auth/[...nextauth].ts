a2c51313436832bd9ae154db3fa4cc2e5c2da6bb
tezarsurya <tzr.surya@gmail.com>
2023-01-25 14:57:35 +0700

    add user to sanity

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,14 +7,57 @@ export const authOptions = {
       clientId: process.env.GH_CLIENT_ID,
       clientSecret: process.env.GH_CLIENT_SECRET,
       authorization: {
-        url: "https://github.com/login/oauth/authorize",
-        params: {
-          scope: "user",
-          redirect_uri: `${process.env.NEXTAUTH_URL}/api/auth/callback/github`,
-        },
+        params: { scope: "user" },
       },
     }),
   ],
+  session: {
+    maxAge: 2 * 24 * 60 * 60,
+  },
+  callbacks: {
+    async signIn({ user }: any) {
+      console.log(user);
+      const mutations = {
+        mutations: [
+          {
+            createIfNotExists: {
+              _id: user.id,
+              _type: "user",
+              email: user.email,
+              name: user.name,
+              image: user.image,
+            },
+          },
+        ],
+      };
+      const checkUser = await fetch(
+        `https://0i0lc7l1.api.sanity.io/v2021-03-25/data/mutate/production`,
+        {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.SANITY_TOKEN}`,
+          },
+          body: JSON.stringify(mutations),
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          return true;
+        })
+        .catch((error) => {
+          console.log(error);
+          return false;
+        });
+
+      return checkUser;
+    },
+    async jwt({ token, account, profile }: any) {
+      console.log(token, account, profile);
+      return token;
+    },
+  },
 };
 
 export default NextAuth(authOptions);
