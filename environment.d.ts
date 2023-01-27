0a66d64556544ce8e565d3b2383bc231b623b4bf
tezarsurya <tzr.surya@gmail.com>
2023-01-28 05:38:50 +0700

    add NEXT_PUBLIC env

--- environment.d.ts
+++ environment.d.ts
@@ -8,6 +8,10 @@ declare global {
       NEXTAUTH_URL: string;
       NEXTAUTH_SECRET: string;
       SANITY_TOKEN: string;
+      NEXT_PUBLIC_SANITY_TOKEN: string;
+      NEXT_PUBLIC_SANITY_PROJECT_ID: string;
+      NEXT_PUBLIC_SANITY_DATASET: string;
+      NEXT_PUBLIC_SANITY_API_VERSION: string;
     }
   }
 }
