1c27079fdbcd4714d99ae66000593c6b97ee29e1
tezarsurya <tzr.surya@gmail.com>
2023-02-14 21:46:05 +0700

    change silly sanity configuration

--- src/sanity.ts
+++ src/sanity.ts
@@ -1,12 +1,10 @@
 import { createClient } from "next-sanity";
 
-const env = process.env.NODE_ENV;
-
 const config = {
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: env === "development" ? "dev" : "production",
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
-  useCdn: true,
+  dataset: "production",
+  apiVersion: "2021-03-25",
+  useCdn: false,
 };
 
 export const sanityClient = createClient(config);
