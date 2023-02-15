001b484594f91b61d5a951fae0cecebaf5258eb3
tezarsurya <tzr.surya@gmail.com>
2023-02-15 07:34:23 +0700

    add title in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,22 +3,29 @@ import TransitionLoading from "@/components/TransitionLoading";
 import UserInput from "@/components/UserInput";
 import Welcome from "@/components/Welcome";
 import { useSession } from "next-auth/react";
+import Head from "next/head";
 
 const App = () => {
   const { status, data } = useSession();
   return (
-    <div className="grid min-h-screen w-full grid-cols-1 bg-veryLightGray">
-      {status === "unauthenticated" ? (
-        <Welcome />
-      ) : status === "loading" ? (
-        <TransitionLoading />
-      ) : (
-        <div className="mx-auto flex h-screen w-full flex-col justify-between gap-8 py-8 px-6 md:w-10/12 md:px-0 lg:w-6/12 2xl:w-5/12">
-          <PostContainer />
-          <UserInput />
-        </div>
-      )}
-    </div>
+    <>
+      <Head>
+        <title>Hehe forum</title>
+      </Head>
+
+      <div className="grid min-h-screen w-full grid-cols-1 bg-veryLightGray">
+        {status === "unauthenticated" ? (
+          <Welcome />
+        ) : status === "loading" ? (
+          <TransitionLoading />
+        ) : (
+          <div className="mx-auto flex h-screen w-full flex-col justify-between gap-2 py-8 px-6 md:w-10/12 md:gap-8 md:px-0 lg:w-6/12 2xl:w-5/12">
+            <PostContainer />
+            <UserInput />
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
